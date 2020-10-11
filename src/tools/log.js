e8db25f325754e203811ce3070ca45afc79e5d0a
Grzegorz Ilnicki <ggilnicki@gmail.com>
2020-10-11 20:48:53 +0200

    Add progress log (self-updatnig line)

--- src/tools/log.js
+++ src/tools/log.js
@@ -12,5 +12,11 @@ exports.error = (...args) => {
     console.error(...args);
 }
 
+exports.progress = (msg, progress) => {
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(msg + ' ' + progress + '%');
+}
+
 
 // module.exports = log;
